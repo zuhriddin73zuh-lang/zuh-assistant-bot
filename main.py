35b95995b4febcd0d497b67d8eb9ecc89303109c
zuhriddin73zuh-lang <zuhriddin73zuh@gmail.com>
2025-10-04 10:11:33 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+import telebot
+import os
+from flask import Flask, request
+
+TOKEN = os.getenv("BOT_TOKEN")  # токен берём из переменной окружения
+bot = telebot.TeleBot(TOKEN)
+app = Flask(__name__)
+
+# команда /start
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Привет! Я твой ассистент-бот 🤖")
+
+# для проверки, что сервер работает
+@app.route('/')
+def index():
+    return "Бот работает!"
+
+# вебхук
+@app.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    json_str = request.stream.read().decode("utf-8")
+    update = telebot.types.Update.de_json(json_str)
+    bot.process_new_updates([update])
+    return "!", 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
